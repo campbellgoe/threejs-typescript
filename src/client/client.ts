d457619b44aa512a72f81fe6e74421a38c298510
George Campbell <campbell.goe@gmail.com>
2024-01-30 10:16:42 +0000

    google not found

--- src/client/client.ts
+++ src/client/client.ts
@@ -2,9 +2,58 @@ import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 import Stats from 'three/examples/jsm/libs/stats.module'
 import { GUI } from 'lil-gui'
-
-
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import { ThreeJSOverlayView } from "@googlemaps/three";
+let map: google.maps.Map;
 const scene = new THREE.Scene()
+const mapOptions = {
+    tilt: 0,
+    heading: 0,
+    zoom: 18,
+    center: { lat: 35.6594945, lng: 139.6999859 },
+    mapId: "15431d2b469f209e",
+    // disable interactions due to animation loop and moveCamera
+    disableDefaultUI: true,
+    gestureHandling: "none",
+    keyboardShortcuts: false,
+  };
+  const mapDiv = document.getElementById("map") as HTMLElement;
+  map = new google.maps.Map(mapDiv, mapOptions);
+  const loader = new GLTFLoader();
+  const url =
+    "https://raw.githubusercontent.com/googlemaps/js-samples/main/assets/pin.gltf";
+
+  loader.load(url, (gltf) => {
+    gltf.scene.scale.set(10, 10, 10);
+    gltf.scene.rotation.x = Math.PI / 2;
+    scene.add(gltf.scene);
+
+    let { tilt, heading, zoom } = mapOptions;
+
+    const animate = () => {
+      if (tilt < 67.5) {
+        tilt += 0.5;
+      } else if (heading <= 360) {
+        heading += 0.2;
+        zoom -= 0.0005;
+      } else {
+        // exit animation loop
+        return;
+      }
+
+      map.moveCamera({ tilt, heading, zoom });
+
+      requestAnimationFrame(animate);
+    };
+
+    requestAnimationFrame(animate);
+  });
+
+  new ThreeJSOverlayView({
+    map,
+    scene,
+    anchor: { ...mapOptions.center, altitude: 100 }
+  });
 scene.add(new THREE.AxesHelper(5))
 
 const camera = new THREE.PerspectiveCamera(
